32019bbd7c779c67d043716bda70ad0689f474a8
Lakkanna Walikar <lakkanna.walikar@hotelsoft.com>
2019-04-19 11:08:03 +0530

    Updates

---
+++ precache-manifest.901208991b390ba59cf97bad047ab238.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f256451a516800470f2",
+    "url": "/Lakkanna/articles/static/js/main.58b4ad3b.chunk.js"
+  },
+  {
+    "revision": "06a26a2d66a020fc2aca",
+    "url": "/Lakkanna/articles/static/js/runtime~main.f691886f.js"
+  },
+  {
+    "revision": "7c70a1fabed55bbbc389",
+    "url": "/Lakkanna/articles/static/js/2.3db61cca.chunk.js"
+  },
+  {
+    "revision": "61facc705975ff8c39ebcc79fbe3bd39",
+    "url": "/Lakkanna/articles/index.html"
+  }
+];
